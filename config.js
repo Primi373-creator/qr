5f2868d1faed72f618f969b80483eef5a373041a
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-04-19 12:05:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,24 @@
+const { Sequelize } = require("sequelize");
+const DB_URL = process.env.DATABASE_URL || "";
+
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://uploader2:uploader2@uploader2.uhnmx1u.mongodb.net/?retryWrites=true&w=majority&appName=uploader2",
-  SESSION_NAME: process.env.SESSION_NAME || "alpha~",
-  ADMIN_KEY: process.env.ADMIN_KEY || "C-iph3r"
+  SESSION_ID: process.env.SESSION_ID || "01A_L_P_H_A_24_04_18_5A9H_I9_U5gd", //your ssid to run bot
+  PORT: process.env.PORT || 3067,
+  ADMIN_KEY: process.env.ADMIN_KEY || "C-iph3r",
+  DATABASE: DB_URL
+    ? new Sequelize(DB_URL, {
+        dialect: "postgres",
+        ssl: true,
+        protocol: "postgres",
+        dialectOptions: {
+          native: true,
+          ssl: { require: true, rejectUnauthorized: false },
+        },
+        logging: false,
+      })
+    : new Sequelize({
+        dialect: "sqlite",
+        storage: "./database.db",
+        logging: false,
+      }),
 };
