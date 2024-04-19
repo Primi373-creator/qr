e66320623e521570567b03ee78676340175e5d49
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-04-19 14:56:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,20 +4,14 @@ const DB_URL = process.env.DATABASE_URL || "";
 module.exports = {
   PORT: process.env.PORT || 3067,
   ADMIN_KEY: process.env.ADMIN_KEY || "C-iph3r",
-  DATABASE: DB_URL
-    ? new Sequelize(DB_URL, {
-        dialect: "postgres",
-        ssl: true,
-        protocol: "postgres",
-        dialectOptions: {
-          native: true,
-          ssl: { require: true, rejectUnauthorized: false },
-        },
-        logging: false,
-      })
-    : new Sequelize({
-        dialect: "sqlite",
-        storage: "./database.db",
-        logging: false,
-      }),
+  DATABASE: new Sequelize(DB_URL, {
+    dialect: "postgres",
+    ssl: true,
+    protocol: "postgres",
+    dialectOptions: {
+      native: true,
+      ssl: { require: true, rejectUnauthorized: false },
+    },
+    logging: false,
+  }),
 };
